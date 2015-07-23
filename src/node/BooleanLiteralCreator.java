e3bc6a5a57cf21cfb47a0666353cb88ad3518387
nettee <xf0718@gmail.com>
2015-07-23 19:37:36 +0800

    Deleted one unnecessary statement in BooleanLiteralCreator

--- src/node/BooleanLiteralCreator.java
+++ src/node/BooleanLiteralCreator.java
@@ -21,7 +21,6 @@ public class BooleanLiteralCreator extends NodeCreator {
 		boolean booleanValue = ((BooleanLiteral) astNode).booleanValue();
 		if (map.get(booleanValue) == null) {
 			Node node = db.createNode(label);
-			node.setProperty("BOOLEAN_VALUE", booleanValue);
 			map.put(booleanValue, node);
 		}
 		return map.get(booleanValue);
