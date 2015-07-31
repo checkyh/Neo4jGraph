d7d50901b1d3220054ba4f589635adc9bfad095a
nettee <xf0718@gmail.com>
2015-07-31 13:01:58 +0800

    Added virtual "Project" node and link all trees together

--- src/run/StoreWorker.java
+++ src/run/StoreWorker.java
@@ -1,5 +1,6 @@
 package run;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -11,8 +12,10 @@ import org.apache.log4j.Logger;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.IBinding;
 import org.eclipse.jdt.core.dom.Type;
+import org.neo4j.graphdb.DynamicLabel;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
 
 import relationship.Rels;
 import ast.ASTCreator;
@@ -29,23 +32,33 @@ public class StoreWorker implements Worker {
 
 		// parse java file to AST
 		ASTCreator creator = new ASTCreator(Option.PROJECT_DIR);
+		
+		List<Node> compilationUnits = new ArrayList<>();
 
 		Iterator<ASTNode> iterator = creator.iterator();
 		while (iterator.hasNext()) {
 			ASTNode root = iterator.next();
-			storeAST(db, root);
+			Node rootNode = storeAST(db, root);
+			compilationUnits.add(rootNode);
+		}
+		
+		Node projectNode = db.createNode(DynamicLabel.label("Project"));
+		for (Node node : compilationUnits) {
+			Relationship rel = projectNode.createRelationshipTo(node, Rels.AST);
+			rel.setProperty("NAME", "FILES");
 		}
 		
 		logger.info("Work finished");
 	}
 
-	public void storeAST(GraphDatabaseService db, ASTNode root) {
+	private Node storeAST(GraphDatabaseService db, ASTNode root) {
 		
 		// store AST into database
 		StoreVisitor visitor = new StoreVisitor(db);
 		root.accept(visitor);
 		List<Type> types = visitor.getTypes();
 		Map<ASTNode, Node> map = visitor.getMap();
+		Node rootNode = map.get(root);
 		
 		// add TypeKey nodes
 		TypeKeyCreator keyNodeCreator = new TypeKeyCreator(db);
@@ -56,6 +69,8 @@ public class StoreWorker implements Worker {
 			Node typeNode = map.get(type);
 			typeNode.createRelationshipTo(keyNode, Rels.KEY);
 		}
+		
+		return rootNode;
 		
 	}
 }
